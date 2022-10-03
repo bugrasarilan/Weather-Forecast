7675f7ea37c9037e48efb17711dc978960f3d377
bugrasarilan <bugrasarilan@gmail.com>
2022-10-03 21:59:53 +0300

    icons

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import {MdOutlineWbSunny} from "react-icons/md"
+import {  BsCloudRainHeavy  } from "react-icons/bs"
+import {  WiDayCloudy  } from "react-icons/wi"
+import {FaRegSnowflake} from "react-icons/fa"
+import {RiWindyFill} from "react-icons/ri"
+
 
 export default function Index() {
-  return <div className='flex'>Home Page</div>;
+  return <div className='flex'>Home Page 
+  <div>
+<h2>güneşli<MdOutlineWbSunny/> </h2>
+<h2>yağmurlu <BsCloudRainHeavy/></h2>
+<h2>parçalı bulutlu<WiDayCloudy/></h2>
+<h2>karlı<FaRegSnowflake/></h2>
+<h2>rüzgarlı<RiWindyFill/></h2>
+</div>
+  
+  </div>;
 }

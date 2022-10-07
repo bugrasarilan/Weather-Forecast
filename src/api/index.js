9827771c261f7cde5618d820c41da0fc60b7f4fc
bugrasarilan <bugrasarilan@gmail.com>
2022-10-07 21:57:30 +0300

    lat,lng

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,15 @@
-import React from "react";
-import axios from "axios";
-const API_KEY = "e7ab5317ec42a1dede35dd7dfe795c29";
+function index() {
+ // const API_KEY: 'e7ab5317ec42a1dede35dd7dfe795c29'
 
 
-export default async function GetWeather(lat, lng) {
-  if (!lat || !lng) {
-    return false;
-  }
-
-  try {
-    const data = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
-    );
-    console.log(data);
-
-    return data;
-  } catch (e) {
-    console.log("e", e);
-    return e;
-  }
+  return (
+    <div>
+     
+    </div>
+  )
 }
+
+export default index
+
+
+
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,15 +4,17 @@ import { BsCloudRainHeavy } from "react-icons/bs";
 import { WiDayCloudy } from "react-icons/wi";
 import { FaRegSnowflake } from "react-icons/fa";
 import { RiWindyFill } from "react-icons/ri";
-import GetWeather from "../../api";
+import axios from "axios";
 
 export default function Index() {
-  
+  const [weather,setweather]=useState();
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
   const [status, setStatus] = useState(null);
+  const API_KEY= "e7ab5317ec42a1dede35dd7dfe795c29"
+ 
 
-  const getLocation = () => {
+  const getLocation =async () => {
     if (!navigator.geolocation) {
       setStatus('Geolocation is not supported by your browser');
     } else {
@@ -21,17 +23,22 @@ export default function Index() {
         setStatus(null);
         setLat(position.coords.latitude);
         setLng(position.coords.longitude);
+        console.log("latitude",position.coords.latitude)
+        console.log("longitude",position.coords.longitude)
+
+      
+         const data = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`)
+         .then(data => {console.log('axios',data)})
+         setweather(data)
+       
       }, () => {
 
-        
-        setStatus('Unable to retrieve your location');
-        
+        setStatus('Unable to retrieve your location');        
         
       });
     }
   }
-
-
+ 
 
   return (
     <div className='flex'>
@@ -59,12 +66,14 @@ export default function Index() {
       </div>
 
       <button onClick={getLocation}>Get Location</button>
-      <h1>Coordinates</h1>
+      <h1>Coordinates </h1>
       <p>{status}</p>
       {lat && <p>Latitude: {lat}</p>}
       {lng && <p>Longitude: {lng}</p>}
-
-
+     
+     <div>
+    
+     </div>
     </div>
   );
 }

d268bafb6b9356405fefab7b797cdecb578f22ab
bugrasarilan <bugrasarilan@gmail.com>
2022-11-04 00:28:28 +0300

    .

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -3,49 +3,170 @@ import axios from "axios";
 import { useState } from "react";
 
 export default function Index() {
-   // const city = "istanbul"
-   const API_KEY = "be9bd55124f63aff70a7e516541d543f";
+
+   const KEY =process.env.REACT_APP_API_KEY; 
    const [day, setDay] = useState([]);
+   const [hour, setHour] = useState([]);
    const [Search, setSearch] = useState("");
 
-
    const cal = (e) => {
-      axios.get(`https://api.openweathermap.org/data/2.5/weatherq=${Search}&lang=tr&appid=${API_KEY}&units=metric`)
-         .then((r) => {
-            console.log("şehir bilgisi ", day)
-            setDay(r.data)
-            console.log("day", r.data)
-            setSearch("")
-         });
+      if (e.key === "Enter")
+         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${Search}&lang=tr&appid=${KEY}&units=metric`)
+            .then((hour) => {
+               console.log("şehir bilgisi ", hour)
+               setHour(hour.data)
+               console.log("setHour", hour)
+               setSearch("")
+
+            });
+
+   };
+
+   const Location = () => {
+      window.navigator.geolocation.getCurrentPosition(
+         (position) => {
+            const lat = position.coords.latitude
+            const lng = position.coords.longitude
+
+            axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${KEY}&units=metric`)
+               .then((r) => {
+                  console.log("5 day 3 hours", day)
+                  setDay(r.data)
+
+               });
+         },
+      );
    };
 
    return (
-      <div className="bg-blue-200">
+      <div className="bg-primary-black">
          detail
-         {/* <button className="text-white bg-gray-400 w-6 h-4 mt-auto rounded-lg hover:bg-gray-500 p-1 " onClick={get}>bilgileri getir</button>
-         <div className="text-2xl "> {day.main && <p>{Math.round(day.main.temp)} </p>} </div>
-         <div className="text-2xl "> {day && <p>{(day.name)} </p>} </div> */}
-         <div>
-            <div>
-               <form >
-                  <input
-                     type="text"
-                     placeholder="Search for places..."
-                     onChange={(e) => setSearch(e.target.value)}
-                     value={Search}
-                  >
-                  </input>
-                  <button onClick={cal}> bul</button>
-               </form>
+         <div className=" text-white">
+            <div >
+               <input className="text-black"
+                  type="text"
+                  placeholder="Search for places..."
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={Search}
+                  onKeyPress={cal}
+               >
+               </input>
+               <button onClick={cal}> bul</button>
+
+            </div>
+            <div className="text-2xl ">  {hour.name}  </div>
+            <div className="text-2xl "> {hour.main && <p>{Math.round(hour.main.temp)} </p>} </div>
+            <div className="text-lg "> {hour.wind && <p> rüzgar hızı: {(hour.wind.speed)}</p>}</div>
+            {hour.weather && <p>hava özelliği: {(hour.weather[0].description)}</p>}
+            {hour.weather && <p>havanın durumu {(hour.weather[0].main)}</p>}
+         </div>
+
+
+
+         <div className=" text-white m-auto  mt-1.5 p-1  flex  ">
+            <div className=""> <button className=" text-white  bg-gray-400 w-60 h-45 mb-5 rounded-lg hover:bg-gray-500 p-1   " onClick={Location}>3 saatlik bilgileri al</button> </div>
+
+            <div className="text-white  m-auto mt-1 mr-3 rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[0].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[0].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[0].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[0].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[0].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[0].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[0].dt * 1000).toLocaleDateString()} </p>}</div>
+
+            </div>
+
+            <div className="  text-white   m-auto mt-1 mr-3 rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[1].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[1].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[1].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[1].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[1].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[1].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[1].dt * 1000).toLocaleDateString()} </p>}</div>
+
+            </div>
+
+            <div className=" text-white  m-auto mt-1. rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[2].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[2].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[2].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[2].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[2].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[2].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[2].dt * 1000).toLocaleDateString()} </p>}</div>
+            </div>
+
+            <div className=" text-white  m-auto mt-1. rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[3].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[3].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[3].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[3].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[3].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[3].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[3].dt * 1000).toLocaleDateString()} </p>}</div>
+            </div>
+
+            <div className=" text-white  m-auto mt-1. rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[4].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[4].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[4].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[4].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[4].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[4].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[4].dt * 1000).toLocaleDateString()} </p>}</div>
+            </div>
+
+            <div className=" text-white  m-auto mt-1. rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[5].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[5].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[5].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[5].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[5].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[5].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[5].dt * 1000).toLocaleDateString()} </p>}</div>
+            </div>
+
+            <div className=" text-white  m-auto mt-1. rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[6].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[6].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[6].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[6].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[6].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[6].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[6].dt * 1000).toLocaleDateString()} </p>}</div>
+            </div>
+
+            <div className=" text-white  m-auto mt-1. rounded-lg p-1 box-content " >
+
+               <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
+               <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[7].main.temp)}ºC</p>}</div>
+               <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[7].main.feels_like)}ºC </p>}</div>
+               <div className="text-lg "> {day.list && <p>nem: {(day.list[7].main.humidity)}%</p>}</div>
+               <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[7].wind.speed)}</p>}</div>
+               <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[7].weather[0].main)}</p>}</div>
+               <div className="text-lg ">  {day.list && <p> time: {(day.list[7].dt_txt)}</p>}  </div>
+               <div> {day.list && <p>Tarih: {new Date(day.list[7].dt * 1000).toLocaleDateString()} </p>}</div>
             </div>
          </div>
       </div>
    )
 };
-
-
-
-
 
 
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
-import { MdOutlineWbSunny } from "react-icons/md";
-import { BsCloudRainHeavy } from "react-icons/bs";
-import { WiDayCloudy } from "react-icons/wi";
-//import { FaRegSnowflake } from "react-icons/fa";
-//import { RiWindyFill } from "react-icons/ri";
 import axios from "axios";
 import "../Home/index.css";
 
 export default function Index() {
   const [status, setStatus] = useState(null);
   const [data, setData] = useState([]);
-  const [iconID, setIconID] = useState();
   const [day, setDay] = useState([]);
+  const [Search, setSearch] = useState("");
+  const [iconID, setIconID] = useState();
 
 
 
-  const API_KEY = "be9bd55124f63aff70a7e516541d543f";
+
+const KEY = process.env.REACT_APP_API_KEY;
+
 
 
   const getLocation = async () => {
@@ -31,19 +29,12 @@ export default function Index() {
           const lng = position.coords.longitude
           console.log("latitude,longitude", lat, lng)
 
-
-
-
-          axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`)
+          axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${KEY}&units=metric`)
             .then((e) => {
               console.log("lat,lng alarak ", data)
               console.log("e.data", e.data.weather[0].id);
               setData(e.data);
-              setIconID(e.data.weather[0].id);
             });
-
-
-
         },
         () => {
           setStatus("Unable to retrieve your location");
@@ -52,110 +43,69 @@ export default function Index() {
     }
   };
 
-  const Location = () => {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const lat = position.coords.latitude
-        const lng = position.coords.longitude
-        
+  const cal = (e) => {
+    if (e.key === "Enter")
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${Search}&lang=tr&appid=${KEY}&units=metric`)
+        .then((day) => {
+          console.log("şehir bilgisi ", day)
+          setDay(day.data)
+          console.log("setday", day)
+          setSearch("")
+          setIconID(day.data.weather[0].main);
+          console.log("main", day.data.weather[0].main);
 
-
-        axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
-          .then((r) => {
-            console.log("5 day 3 hours", day)
-            setDay(r.data)
-
-          });
-
-
-      },
-
-    );
+        });
   };
-
-
-
-
-
-
-
-
-
-
-  //800 clear  500,501 rain
-  // 802, 803, 804 clouds
 
 
   const GetIcon = () => {
-    switch (data.weather[0].id) {
-      case 800: <MdOutlineWbSunny />
-        return;
+    switch (iconID) {
+      case "Clear":
+        return <img src={require(`../../images/01d.png`)} alt="sunny"></img>
 
-      case 801: <WiDayCloudy />
-        return;
+      case "Clouds":
+        return <img src={require(`../../images/03d.png`)} alt="Clouds"></img>
 
-      case 804:
-        // clouds bulutlu//;
-        break;
-      case 500:
-        <BsCloudRainHeavy />
-        break;
+      case "Rain":
+        return <img src={require(`../../images/09d.png`)} alt="Rain"></img>
+
+      case "Thunderstorm":
+        return <img src={require(`../../images/11d.png`)} alt="Thunderstorm"></img>
+
+      case "Drizzle":
+        return <img src={require(`../../images/09d.png`)} alt="Drizzle"></img>
+
+      case "Snow":
+        return <img src={require(`../../images/13d.png`)} alt="Snow"></img>
+
+      case "Mist":
+      case "Fog":
+      case "Smoke":
+      case "Haze":
+      case "Dust":
+      case "Sand":
+      case "Ash":
+      case "Squall":
+      case "Tornado":
+        return <img src={require(`../../images/50d.png`)} alt="alotof"></img>
 
       default:
+
         break;
     }
-
   };
-
-
-
 
   return (
 
     <div className='flex bg-primary-black h-100vh '>
       Home Page
-      {/* <div>
-        <h2>
-          güneşli
-          <MdOutlineWbSunny />{" "}
-        </h2>
-        <h2>
-          yağmurlu <BsCloudRainHeavy />
-        </h2>
-        <h2>
-          parçalı bulutlu
-          <WiDayCloudy />
-        </h2>
-        <h2>
-          karlı
-          <FaRegSnowflake />
-        </h2>
-        <h2>
-          rüzgarlı
-          <RiWindyFill />
-        </h2>
-      </div>  
-      <button className="bg-blue-100 w-50 h-20" onClick={getLocation}>konum bilgilerini al</button>
-     <div >
-        
+
+      <div className=" text-white mt-1.5 rounded-lg p-1 box-content ">
+        <button className="text-white bg-blue-300 w-60 h-45 mt-auto rounded-lg hover:bg-blue-400 p-1 " onClick={getLocation}>Anlık bilgileri al</button>
         <p>{status}</p>
 
-        {data.name && <p> şehir: {data.name} </p>}
-        {data.sys && <p> ülke: {(data.sys.country)} </p>}
-        {data.main && <p> sıcaklık: {Math.ceil(data.main.temp)}ºC </p>}
-        {data.main && <p> nem: {(data.main.humidity)}% </p>}
-        {data.main && <p> basınç: {(data.main.pressure)} hPa </p>}
-        {data.weather && (<p> havanın durumu:{data.weather.map((data) => data.main)}</p>)} 
-        {data.weather && <p> havanın özelliği:{data.weather.map(data => data.description)}  </p>}
-        {data.wind && <p> rüzgar hızı:{(data.wind.speed)}</p> } 
-
-      </div >    */}
-
-      <div className="text-white  m-auto mt-1.5 rounded-lg p-1 box-content ">
-        <button className="text-white bg-gray-400 w-60 h-45 mt-auto rounded-lg hover:bg-gray-500 p-1 " onClick={getLocation}>Anlık bilgileri al</button>
-        <p>{status}</p>
-        <div className="float-right text-4xl p" > {iconID && <GetIcon />}</div>
-        <div className="text-2xl  " >{data.name && <p>  {data.name} </p>}</div>
+        <div className="text-2xl  " >{data.name && <p>  {data.name} </p>}</div> 
+       {/* <div> <img src={require(`../../images/${data.weather && (data.weather[0].main)}.png`)} alt="Snow" ></img></div> */}
         <div className="text-lg ">{data.main && <p> sıcaklık: {Math.ceil(data.main.temp)}ºC  </p>}</div>
         <div className="text-lg " >{data.main && <p> hissedilen sıcaklık: {Math.ceil(data.main.feels_like)}ºC </p>}</div>
         <div className="text-lg "> {data.wind && <p> rüzgar hızı: {(data.wind.speed)}</p>}</div>
@@ -164,60 +114,45 @@ export default function Index() {
         {data.weather && (<p> havanın durumu:{data.weather.map((data) => data.main)}</p>)}
 
         {data.weather && <p>weather id: {(data.weather[0].id)}</p>}
-
-
-
-
-      </div>
-      <div className=" text-white m-auto  mt-1.5 p-1  flex  ">
-              <div className=""> <button className=" text-white  bg-gray-400 w-60 h-45 mb-5 rounded-lg hover:bg-gray-500 p-1   " onClick={Location}>3 saatlik bilgileri al</button> </div>
-
-        {/* <p>{status}</p> */}
-
-
-        <div className="text-white  m-auto mt-1 mr-3 rounded-lg p-1 box-content " >
-          <div className="float-right text-4xl p" > {iconID && <GetIcon />}</div>
-          <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
-          <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[0].main.temp)}ºC</p>}</div>
-          <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[0].main.feels_like)}ºC </p>}</div>
-          <div className="text-lg "> {day.list && <p>nem: {(day.list[0].main.humidity)}%</p>}</div>
-          <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[0].wind.speed)}</p>}</div>
-          <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[0].weather[0].main)}</p>}</div>
-          <div className="text-lg ">  {day.list && <p> time: {(day.list[0].dt_txt)}</p>}  </div>
-          <div> {day.list && <p>Tarih: {new Date(day.list[0].dt * 1000).toLocaleDateString()} </p>}</div>
-
-        </div>
-
-        <div className="  text-white   m-auto mt-1 mr-3 rounded-lg p-1 box-content " >
-          <div className="float-right text-4xl p" > {iconID && <GetIcon />}</div>
-          <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
-          <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[1].main.temp)}ºC</p>}</div>
-          <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[1].main.feels_like)}ºC </p>}</div>
-          <div className="text-lg "> {day.list && <p>nem: {(day.list[1].main.humidity)}%</p>}</div>
-          <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[1].wind.speed)}</p>}</div>
-          <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[1].weather[0].main)}</p>}</div>
-          <div className="text-lg ">  {day.list && <p> time: {(day.list[1].dt_txt)}</p>}  </div>
-          <div> {day.list && <p>Tarih: {new Date(day.list[0].dt * 1000).toLocaleDateString()} </p>}</div>
-
-        </div>
-
-        <div className=" text-white  m-auto mt-1. rounded-lg p-1 box-content " >
-          <div className="float-right text-4xl p" > {iconID && <GetIcon />}</div>
-          <div className="text-2xl"> {day.city && <p>{(day.city.name)} </p>} </div>
-          <div className="text-lg ">{day.list && <p>sıcaklık: {Math.ceil(day.list[2].main.temp)}ºC</p>}</div>
-          <div className="text-lg " >{day.list && <p> hissedilen sıcaklık: {Math.ceil(day.list[2].main.feels_like)}ºC </p>}</div>
-          <div className="text-lg "> {day.list && <p>nem: {(day.list[2].main.humidity)}%</p>}</div>
-          <div className="text-lg " >  {day.list && <p>rüzgar hızı: {(day.list[2].wind.speed)}</p>}</div>
-          <div className="text-lg " > {day.list && <p>havanın durumu: {(day.list[2].weather[0].main)}</p>}</div>
-          <div className="text-lg ">  {day.list && <p> time: {(day.list[2].dt_txt)}</p>}  </div>
-          <div> {day.list && <p>Tarih: {new Date(day.list[0].dt * 1000).toLocaleDateString()} </p>}</div>
-        </div>
+        {data.weather && <p>hava özelliği: {(data.weather[0].description)}</p>}
+       
+{/* {day.weather && (day.weather[0].main)} */}
 
       </div>
+      <div className=" text-white mt-1.5 p-1 text-center ">
+        <div >
+          <input className="text-black "
+            type="text"
+            placeholder="Search for places..."
+            onChange={(e) => setSearch(e.target.value)}
+            value={Search}
+            onKeyPress={cal}
+          >
 
+          </input>
+      
+        </div>
+
+        <div className=" m-auto  text-center  mt-2 ">
+        <div className="text-4xl text-center ">  {day.name}  </div>
+
+
+        <div className=" flex justify-center">{GetIcon()}</div>
+        <div className="  ">{day.weather && <p> {(day.weather[0].description)}</p>}</div>
+        <div className="">{day.main && <p> sıcaklık: {Math.round(day.main.temp)} ºC </p>} </div>
+        <div className="float-left">{day.main && <p> min sıcaklık: {Math.round(day.main.temp_min)} ºC </p>} </div>
+        <div className="ml-1 float-right">{day.main && <p> max sıcaklık: {Math.round(day.main.temp_max)} ºC </p>} </div>
+
+        <div className="">{day.main && <p>Nem: {Math.round(day.main.humidity)} % </p>} </div>
+
+        <div className=" "> {day.wind && <p> rüzgar hızı: {(day.wind.speed)}</p>}</div>
+        <div> {day.dt && <p>Tarih: {new Date(day.dt * 1000).toLocaleDateString()} </p>}</div>
+   
+       </div>
+      </div>
     </div>
 
 
 
   )
-}
+};

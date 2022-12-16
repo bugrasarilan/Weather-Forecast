ce39a5182366bc15497a15ff72111291bdb1ad73
bugrasarilan <bugrasarilan@gmail.com>
2022-12-16 18:03:00 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="Detail" element={<Detail />} />
-      </Routes>
+      </Routes>  
     </div>
   );
 }
